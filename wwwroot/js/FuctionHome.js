4a0ee849ceff6cdeb3ac821bdf42ceb6fb1d886d
Deivit Agudelo <deivit.agu@gmail.com>
2026-06-19 17:08:44 -0500

    Delete wwwroot directory

--- wwwroot/js/FuctionHome.js
+++
@@ -1,9 +0,0 @@
-﻿window.copiarContenido = (texto) => {
-
-    if (texto === "") {
-        alert("Elemento no copiado")
-    } else {
-        alert("Elemento copiado")
-    }
-    navigator.clipboard.writeText(texto);
-};
